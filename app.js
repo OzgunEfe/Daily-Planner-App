de09dd0d37f5d45c7a362b2fa420b9085159c552
OzgunEfe <ozgunefe.dev@gmail.com>
2022-12-20 22:02:14 +0000

    I change some logic

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // This function writes the current day time
-var currentDay = moment().format('dddd, MMMM Do');
+var currentDay = moment().format("dddd, MMMM Do");
 
-$('#currentDay').text(currentDay);
+$("#currentDay").text(currentDay);
 
 // This function writes the time blocks
-var time = moment(09, 'HH');
+var time = moment(09, "HH");
 
-while(time.hour() < 17) {
-    $('body').append(`
-    <main>
-    <span class="hour">${time.format('hA')}</span>
-    <textarea id="${time.format('H')}" class="time-block" name="text-area"></textarea>
+while (time.hour() < 23) {
+  $("body").append(`
+    <section>
+    <span class="hour">${time.format("hA")}</span>
+    <textarea id="${time.format(
+      "H"
+    )}" class="time-block" name="text-area"></textarea>
     <button class="saveBtn">
       <img
         src="./assets/save-icon.png"
@@ -19,24 +21,77 @@ while(time.hour() < 17) {
         width="32px"
       />
     </button>
-    </main>
+    </section>
     `);
 
-    time.add(1, 'hours');
-};
-
-// This condition checks past, future and current time
-var currentTime = Number(moment().format('H'));
- 
-for(i=9; i<17; i++){
-    if (currentTime == i){
-        $(`#${i}`).addClass('present');
-    } else if(currentTime > i){
-        $(`#${i}`).addClass('past');
-    } else if (currentTime < i){
-        $(`#${i}`).addClass('future');
-    }
+  time.add(1, "hours");
 }
 
+// This condition checks past, future and current time
+var currentTime = Number(moment().format("H"));
 
+// currentTime = 10;
 
+for (i = 9; i < 23; i++) {
+  if (currentTime == i) {
+    $(`#${i}`).addClass("present");
+  } else if (currentTime > i) {
+    $(`#${i}`).addClass("past");
+  } else if (currentTime < i) {
+    $(`#${i}`).addClass("future");
+  }
+}
+
+taskList = [];
+
+// Local Storage Functions
+function saveTasks(arr) {
+  localStorage.setItem("Tasks", JSON.stringify(arr));
+}
+
+function hideNotification() {
+  saveTaskNotification.addClass("hide");
+}
+
+// Notification text
+var saveTaskNotification = $(".notification");
+
+// Save button function
+$(".saveBtn").on("click", function () {
+  saveTaskNotification.removeClass("hide");
+  setTimeout(hideNotification, 3000);
+
+  // Text Area
+  var textArea = $("textarea");
+
+  // Get the textarea ID and value
+  textArea.change(function () {
+    var textareaId = textArea.id;
+    var textareaValue = textArea.val();
+
+    var textareaObject = {
+      id: textareaId,
+      task: textareaValue
+    };
+
+		taskList.push(textareaObject);
+  });
+
+	saveTasks(taskList);
+});
+
+function displayTasks() {
+  var taskList = JSON.parse(localStorage.getItem("Tasks")) || [];
+
+  taskList.forEach(function (taskList) {
+    $(`#${taskList.id}`).text(taskList.task);
+  });
+}
+
+// // tiklanan text alaninin id'sini aliyor.
+// $("textarea").click(function () {
+//   var id = $(this).attr("id");
+
+// // textarea value aliyor.
+// $("textarea").change(function () {
+// 	var text = $(this).val();
